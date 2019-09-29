1a874ed30cddb3ddaf955b0e981fd2b41b91c11a
Scuuter <class.object@yandex.ru>
2019-09-29 10:51:16 +0300

    add CreateTaskServlet

---
+++ src/main/java/VolunteerAppProject/Servlets/Tasks/CreateTaskServlet.java
@@ -0,0 +1,49 @@
+package VolunteerAppProject.Servlets.Tasks;
+
+import VolunteerAppProject.Bot.BotController;
+import VolunteerAppProject.ServerStarter;
+import com.fasterxml.jackson.core.JsonEncoding;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CreateTaskServlet", urlPatterns = { "/api/tasks/create" })
+public class CreateTaskServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+        response.getWriter().println("OK");
+
+        request.getParts();
+
+        System.out.println("POST");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        String authToken = request.getParameter("auth");
+        String responseString = "";
+
+
+        if (authToken != null && authToken.equals(ServerStarter.token())) {
+            responseString = "OK";
+            BotController botController = ServerStarter.getBotController();
+            botController.broadcast(request.getParameter("message"));
+        }
+        else
+            responseString = ServerStarter.getAccessDeniedResponce();
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setCharacterEncoding(JsonEncoding.UTF8.getJavaName());
+        response.getWriter().println(responseString);
+    }
+
+}
