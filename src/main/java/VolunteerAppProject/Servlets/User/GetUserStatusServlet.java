bb5dd84f1620d82a28aadd33fd85fcd0ad52bad9
Ivanets Aleksandr <ivanetsas@yandex.ru>
2019-09-29 03:06:02 +0300

    getUserStatusServlet

---
+++ src/main/java/VolunteerAppProject/Servlets/User/GetUserStatusServlet.java
@@ -0,0 +1,70 @@
+package VolunteerAppProject.Servlets.User;
+
+import VolunteerAppProject.ServerStarter;
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@WebServlet(name = "GetUserStatusServlet", urlPatterns = { "api/user/getUserStatus" })
+public class GetUserStatusServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+        response.getWriter().println("OK");
+
+        request.getParts();
+
+        System.out.println("POST");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        String authToken = request.getParameter("auth");
+        String responceString = "";
+
+        String vkId = request.getParameter("vk_id");
+
+
+        if (authToken != null && authToken.equals(ServerStarter.token()))
+            responceString = getUserStatusJson();
+        else
+            responceString = ServerStarter.getAccessDeniedResponce();
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setCharacterEncoding(JsonEncoding.UTF8.getJavaName());
+        response.getWriter().println(responceString);
+    }
+
+    private String getUserStatusJson(){
+        int status = 1;
+
+        try {
+            JsonFactory jsonFactory = new JsonFactory();
+            OutputStream outputStream = new ByteArrayOutputStream();
+            JsonGenerator jsonGenerator = jsonFactory.createGenerator(outputStream, JsonEncoding.UTF8); // or Stream, Reader
+            jsonGenerator.writeStartObject();
+
+            jsonGenerator.writeStringField("user_status", String.valueOf(status));
+
+            jsonGenerator.writeEndObject();
+            jsonGenerator.close();
+
+            return outputStream.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "<error>Server error of creating json</error>";
+        }
+    }
+}
